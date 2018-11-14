5a728d3b7578231a6dadf9aa82c5d3859ec40f7d
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2018-11-14 13:55:30 +0100

    2018-11-14

--- Assets/_GameAssets/Scripts/FernanditoScript.cs
+++ Assets/_GameAssets/Scripts/FernanditoScript.cs
@@ -4,36 +4,82 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class FernanditoScript : MonoBehaviour {
+    enum Estado { Idle, Andando, Corriendo, Saltando, Disparando };
+    Estado estado = Estado.Idle;
+
     public GameObject targetCircle;
     public Camera camara;
+    public LayerMask walkableLayer;
 
     Animator animador;
     NavMeshAgent agente;
-	void Start () {
+    void Start() {
         agente = GetComponent<NavMeshAgent>();
         animador = GetComponent<Animator>();
-	}
-	
-	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
             ManageMouseClick();
         }
-        print(agente.remainingDistance);
-        if (agente.remainingDistance <= agente.stoppingDistance) {
-            animador.SetBool("andando", false);
+        switch (estado) {
+            case Estado.Idle:
+                //NO HAGO NADA
+                break;
+            case Estado.Andando:
+                ComprobarDestino();
+                break;
+            case Estado.Saltando:
+
+                break;
+            case Estado.Corriendo:
+
+                break;
+            case Estado.Disparando:
+
+                break;
         }
-	}
+    }
+
+    private void ComprobarDestino() {
+        if (!agente.pathPending) {
+            if (agente.remainingDistance <= agente.stoppingDistance) {
+                animador.SetBool("andando", false);
+                estado = Estado.Idle;
+            }
+        }
+    }
 
     private void ManageMouseClick() {
         Ray ray = camara.ScreenPointToRay(Input.mousePosition);
         RaycastHit rch;
-        bool hasTouch = Physics.Raycast(ray, out rch);
+        bool hasTouch = Physics.Raycast(ray, out rch, Mathf.Infinity, walkableLayer);
         if (hasTouch) {
-            targetCircle.transform.position = rch.point;
-            targetCircle.transform.rotation = Quaternion.LookRotation(rch.normal);
-            agente.destination = targetCircle.transform.position;
-            animador.SetBool("andando", true);
+            switch (estado) {
+                case Estado.Idle:
+                    Andar(rch);
+                    break;
+                case Estado.Andando:
+
+                    break;
+                case Estado.Saltando:
+
+                    break;
+                case Estado.Corriendo:
+
+                    break;
+                case Estado.Disparando:
+
+                    break;
+            }
         }
+    }
+    private void Andar(RaycastHit rch) {
+        targetCircle.transform.position = rch.point;
+        targetCircle.transform.rotation = Quaternion.LookRotation(rch.normal);
+        agente.destination = targetCircle.transform.position;
+        animador.SetBool("andando", true);
+        estado = Estado.Andando;
     }
 
 }
---
+++ Assets/_GameAssets/Scripts/LlaveScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LlaveScript : MonoBehaviour {
+    public Animator animatorPuerta;
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.name == "Fernandito") {
+            AbrirPuerta();
+        }
+    }
+    private void AbrirPuerta() {
+        animatorPuerta.SetBool("AbreteSesamo", true);
+        Destroy(gameObject);
+    }
+}
