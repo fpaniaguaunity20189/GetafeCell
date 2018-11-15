460751a7a19bb9050d145c8a3f67aa1a61640ae2
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2018-11-15 10:24:15 +0100

    2018-11-15 10:23
    SUBIDA PARCIAL

--- Assets/_GameAssets/Scripts/LlaveScript.cs
+++ Assets/_GameAssets/Scripts/LlaveScript.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LlaveScript : MonoBehaviour {
     public Animator animatorPuerta;
+    public VigilanteScript vs;
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.name == "Fernandito") {
             AbrirPuerta();
         }
     }
     private void AbrirPuerta() {
+        vs.SetTarget(transform.position);
         animatorPuerta.SetBool("AbreteSesamo", true);
         Destroy(gameObject);
     }
---
+++ Assets/_GameAssets/Scripts/VigilanteScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class VigilanteScript : MonoBehaviour {
+    public Transform[] puntosPatrulla = new Transform[4];
+    NavMeshAgent agente;
+    enum Estado { Idle, Andando, Corriendo, Saltando, Disparando };
+    Estado estado = Estado.Idle;
+    const int TIEMPO_ESPERA = 1;//TIEMPO DE ESPERA ENTRE ASIGNACIONES DE PUNTOS DE PATRULLA
+
+    void Start () {
+        agente = GetComponent<NavMeshAgent>();
+        AsignarPuntoPatrulla();
+    }
+    void Update () {
+        switch (estado) {
+            case Estado.Idle:
+                //NO HAGO NADA
+                break;
+            case Estado.Andando:
+                ComprobarDestino();
+                break;
+            case Estado.Saltando:
+
+                break;
+            case Estado.Corriendo:
+
+                break;
+            case Estado.Disparando:
+
+                break;
+        }
+    }
+
+    private void ComprobarDestino() {
+        if (!agente.pathPending) {
+            if (agente.remainingDistance <= agente.stoppingDistance+0.1) {
+                //animador.SetBool("andando", false);
+                estado = Estado.Idle;
+                Invoke("AsignarPuntoPatrulla", TIEMPO_ESPERA);
+            }
+        }
+    }
+    private void AsignarPuntoPatrulla() {
+        int pp = Random.Range(0, puntosPatrulla.Length);
+        agente.destination = puntosPatrulla[pp].position;
+        estado = Estado.Andando;
+    }
+    public void SetTarget(Vector3 position) {
+        agente.destination = position;
+        estado = Estado.Andando;
+    }
+    /* ASIGNACION DE PUNTOS DE PATRULLA SECUENCIAL
+    int pp = 0;
+    private void AsignarPuntoPatrulla() {
+        if (pp == puntosPatrulla.Length) pp = 0;
+        agente.destination = puntosPatrulla[pp].position;
+        estado = Estado.Andando;
+        pp++;
+    }
+    */
+}
